def4d26639b49eb95ce11c0ade1065bfbcf46965
Salim Ozel <salimozel123@gmail.com>
2023-07-21 10:49:52 +0300

    last changes

--- Swift/ListApp/ListApp/Controllers/ViewController.swift
+++ Swift/ListApp/ListApp/Controllers/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource  {
+   
+    @IBOutlet weak var tableView : UITableView!
+    
+    let data = ["Swift","Kotlim","Flutter","C#","Python","C","C++"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
 
 }
 
