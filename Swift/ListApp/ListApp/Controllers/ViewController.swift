8070b651f4ca3866cdde6fd00d0614793aab1c2f
Salim Ozel <salimozel123@gmail.com>
2023-07-22 13:56:34 +0300

    CoreData transactions

--- Swift/ListApp/ListApp/Controllers/ViewController.swift
+++ Swift/ListApp/ListApp/Controllers/ViewController.swift
@@ -6,35 +6,33 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource  {
+class ViewController: UIViewController{
     var alertController = UIAlertController()
     @IBOutlet weak var tableView : UITableView!
-    var data = [String]()
+    var data = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
-        cell.textLabel?.text = data[indexPath.row]
-        return cell
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        fetch()
+        
     }
     @IBAction func didRemoveBarButtonItemTabbed(_ sender : UIBarButtonItem){
-        presentAlert(title: "Uyarı!", message: "Listedki bütün ögeleri silmek istediğinizden emin misiniz ? ", cancelButtonTitle: "Vazgeç", defaultButtonTitle: "Evet") { _ in
-            self.data.removeAll()
-            self.tableView.reloadData()
+        presentAlert(title: "Uyarı!",
+                     message: "Listedki bütün ögeleri silmek istediğinizden emin misiniz ? ",
+                     cancelButtonTitle: "Vazgeç",
+                     defaultButtonTitle: "Evet") { _ in
+            // all items deleting
+            let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            let managedObjectContext = appDelegate?.persistentContainer.viewContext
+            for item in self.data {managedObjectContext?.delete(item)}
+            try? managedObjectContext?.save()
+            self.fetch()
         }
-        
-    
     }
-    
     @IBAction func didAddBarButtonItemTabbed(_ sender : UIBarButtonItem){
         presentAddAlert()
     }
@@ -48,8 +46,15 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
             let text = self.alertController.textFields?.first?.text
             
             if text != ""{
-                self.data.append((text)!)
-                self.tableView.reloadData()
+                let appDelegate = UIApplication.shared.delegate as? AppDelegate
+                let managedObjectContext = appDelegate?.persistentContainer.viewContext
+                let entity = NSEntityDescription.entity(forEntityName: "ListItem",
+                                                        in: managedObjectContext! )
+                let listItem = NSManagedObject(entity: entity!,
+                                               insertInto: managedObjectContext)
+                listItem.setValue(text , forKey: "title")
+                try? managedObjectContext?.save()
+                self.fetch()
             }
             else {
                 self.presentWarningAlert()
@@ -87,6 +92,76 @@ class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSourc
         alertController.addAction(canceButton)
         present(alertController, animated: true)
     }
-    
+    func fetch () {
+        
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        let managedObjectContext = appDelegate?.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "ListItem")
+        
+        data = try! managedObjectContext!.fetch(fetchRequest)
+        
+        tableView.reloadData()
+        
+    }
 }
 
+extension ViewController : UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell",
+                                                 for: indexPath)
+        let listItem = data[indexPath.row]
+        cell.textLabel?.text = listItem.value(forKey: "title") as? String
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .normal,
+                                              title: "Sil") { _, _, _ in
+            let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            
+            let managedObjectContext = appDelegate?.persistentContainer.viewContext
+            
+            managedObjectContext?.delete(self.data[indexPath.row])
+            
+            try? managedObjectContext?.save()
+            
+            self.fetch()
+        }
+        deleteAction.backgroundColor = .systemRed
+        
+        let editAction = UIContextualAction(style: .normal,
+                                            title: "Düzenle") { _, _, _ in
+                self.presentAlert(title: "Elemanı Düzenle",
+                                  message: nil,
+                                  cancelButtonTitle: "Vazgeç",
+                                  defaultButtonTitle: "Düzenle",
+                                  isTextFieldAvaliable: true,
+                                  defaultButtonHandler: {_ in
+                    let text = self.alertController.textFields?.first?.text
+                    if text != ""{
+                        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+                        
+                        let managedObjectContext = appDelegate?.persistentContainer.viewContext
+                        self.data[indexPath.row].setValue(text, forKey:"title")
+                        if managedObjectContext!.hasChanges{
+                            try? managedObjectContext?.save()
+                        }
+                        self.tableView.reloadData()
+                    }
+                    else {
+                        self.presentWarningAlert()
+                    }
+                }
+                                  
+                )
+            
+        }
+        let config = UISwipeActionsConfiguration(actions:[deleteAction, editAction])
+        return config
+    }
+}
